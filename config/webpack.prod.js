4f070671a157361f5e2a3973cae5ece8f9690004
Andrey Bilchenko <dev.andrey.bilchenko@gmail.com>
2020-12-05 22:07:48 +0200

    update build logic

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,59 +1,17 @@
 const paths = require('./paths');
-const {merge} = require('webpack-merge');
-const common = require('./webpack.common.js');
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-
-module.exports = merge(common, {
-    mode: 'production',
-    devtool: false,
-    entry: [paths.src + '/screen-scrolling-js.js'],
+module.exports = {
+    entry: paths.src + '/screen-scrolling-js.js',
     output: {
         path: paths.build,
-        publicPath: './',
-        filename: '[name].js',
+        filename: 'screen-scrolling-js.js',
+        library: 'screen-scrolling-js',
+        libraryTarget: 'umd',
     },
-    plugins: [
-        // Extracts CSS into separate files
-        // Note: style-loader is for development, MiniCssExtractPlugin is for production
-        // Извлекать CSS в отдельные файлы
-        // Обратите внимание, что style-loader предназначен для разработки, а MiniCssExtractPlugin - для продакшна
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
-    ],
+    plugins: [],
     module: {
         rules: [
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 2,
-                            sourceMap: false,
-                        },
-                    },
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
+            {test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']},
         ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
-        runtimeChunk: {
-            name: 'runtime',
-        },
-    },
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
-    },
-});
+    }
+};
